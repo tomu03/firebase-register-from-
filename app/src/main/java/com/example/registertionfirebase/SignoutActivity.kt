660c13779d24406e6b767de0a730131ddd326fa0
tomu03 <369tomu@gamil.com>
2024-05-18 10:32:04 +0600

    layout are added

---
+++ app/src/main/java/com/example/registertionfirebase/SignoutActivity.kt
@@ -0,0 +1,15 @@
+package com.example.registertionfirebase
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signout)
+
+    }
+}
---
+++ app/src/main/java/com/example/registertionfirebase/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.registertionfirebase
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+    }
+}
