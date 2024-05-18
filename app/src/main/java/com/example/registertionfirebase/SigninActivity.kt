9e811861eb60130138892d2ae04f4e1099ef62e6
tomu03 <369tomu@gamil.com>
2024-05-18 12:32:03 +0600

    Done

--- app/src/main/java/com/example/registertionfirebase/SigninActivity.kt
+++ app/src/main/java/com/example/registertionfirebase/SigninActivity.kt
@@ -2,16 +2,11 @@ package com.example.registertionfirebase
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Patterns
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import com.example.registertionfirebase.ViewModels.AuthViewModel
 import com.example.registertionfirebase.databinding.ActivitySigninBinding
-import com.example.registertionfirebase.databinding.ActivitySignupBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class SigninActivity : AppCompatActivity() {
@@ -24,15 +19,14 @@ class SigninActivity : AppCompatActivity() {
 
             viewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
 
-            binding.signinBtm.setOnClickListener {
+            binding.signinBtn.setOnClickListener {
                 val email = binding.emailEt.text.toString()
                 val password = binding.passwordEt.text.toString()
-                if (email.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(this, "Please fill up all the fields!!!", Toast.LENGTH_SHORT)
-                        .show()
-                } else {
-                    viewModel.signup(email, password).observe(this, {result->
-                        Toast.makeText(this, result, Toast.LENGTH_SHORT).show()
+                if(email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(this,"Please fill up all the fields!!!", Toast.LENGTH_SHORT).show()
+                }else{
+                    viewModel.signIn(email,password).observe(this, {result ->
+                        Toast.makeText(this,result, Toast.LENGTH_SHORT).show()
                         if(it.equals("Sign in successful")){
                             startActivity(Intent(this, SignoutActivity::class.java))
                         }
